6e10e0df5e631b0d80fc832b4062c23bef714aff
Savolainen Pasi <pasi.savolainen@opuscapita.com>
2018-01-15 16:43:49 +0200

    Fail first

---
+++ InterfaceLoggerTests/Basics.cs
@@ -0,0 +1,19 @@
+﻿using InterfaceLogger;
+using Xunit;
+
+namespace InterfaceLoggerTests
+{
+    public interface IMyTestLoggerInterface
+    {
+    }
+    public class Basics
+    {
+        [Fact]
+        public void Creates()
+        {
+            var mahLogger = LoggerManager.Get<IMyTestLoggerInterface>();
+
+            Assert.NotNull(mahLogger);
+        }
+    }
+}
---
+++ src/LoggerManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceLogger
+{
+    public class LoggerManager
+    {
+        public static TInterface Get<TInterface>()
+        {
+            return default(TInterface);
+        }
+    }
+}
